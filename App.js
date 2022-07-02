21b175f31010f0d94abc17060a54efae6606dda6
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-07-02 19:41:26 +0530

    installed react-navigation/native, react-native-screens, react-native-safe-area-context, @react-navigation/native-stack

--- App.js
+++ App.js
@@ -2,12 +2,20 @@ import React from "react";
 import News from "./components/News";
 import { NativeBaseProvider, Box } from "native-base";
 import NavigationTab from "./components/NavigationTab";
-import { BrowserRouter, Router, Rotes } from "react-router-dom";
+// import { BrowserRouter, Router, Routes } from "react-router-dom";
+import Test from "./components/test";
+
 export default function App() {
   return (
     <NativeBaseProvider>
+      <Test />
       <NavigationTab />
 
+      {/* <BrowserRouter>
+                <Router>
+          <Routes path="/general" element={<News />} />
+        </Router>
+      </BrowserRouter> */}
       <News />
     </NativeBaseProvider>
   );
