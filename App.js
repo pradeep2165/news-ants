8b07777f5e667a4ff8417c208876d4e8e9c4e455
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-07-02 17:56:28 +0530

    react router added

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
 import React from "react";
-import { Text, Link, HStack, Center, Heading, Switch, useColorMode, NativeBaseProvider, extendTheme, Container } from "native-base";
+import { NativeBaseProvider, Container } from "native-base";
 // import NavigationTab from "./components/NavigationTab";
 import News from "./components/News";
 
 export default function App() {
   return (
     <NativeBaseProvider>
-      {/* <NavigationTab /> */}
-      <Center>
-        <Container>
-          <Heading>
-            <Text color="emerald.500"> NewsAnts</Text>
-          </Heading>
-        </Container>
-      </Center>
       <News />
     </NativeBaseProvider>
   );
--- components/News.js
+++ components/News.js
@@ -1,19 +1,23 @@
 import React from "react";
-import { Text, Link, HStack, Center, Heading, Switch, useColorMode, NativeBaseProvider, extendTheme, Container } from "native-base";
+import { Text, Link, HStack, Center, Heading, Switch, useColorMode, NativeBaseProvider, extendTheme, Container, Box, Stack } from "native-base";
 // import NavigationTab from "./components/NavigationTab";
 import NewsItems from "./NewsItems";
 
 export default function News() {
+  const rawdata = require("../rawdata.json");
+  const articles = rawdata.articles;
+  console.log(articles);
+  document.title = "NewsAnts";
   return (
-    <>
-      <Center>
-        <Container>
-          <Heading>
-            <Text color="emerald.500"> NewsAnts</Text>
-          </Heading>
-        </Container>
-      </Center>
-      <NewsItems />
-    </>
+    <Box>
+      <Heading textAlign="center" bg="dark.100" p="5" color="white">
+        <Text> NewsAnts</Text>
+      </Heading>
+      <Stack flexWrap="wrap" flexDirection={"row"} justifyContent="center">
+        {articles.map((items, index) => {
+          return <NewsItems news={items} key={index} />;
+        })}
+      </Stack>
+    </Box>
   );
 }
