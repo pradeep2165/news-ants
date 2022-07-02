c7ac9257bd03db49934c128dbde6d7104381d730
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-07-02 17:26:37 +0530

    rowdata inserted and showed in web

--- components/NewsItems.js
+++ components/NewsItems.js
@@ -1,15 +1,87 @@
 import React from "react";
-import { Text, Link, HStack, Center, Heading, Switch, useColorMode, NativeBaseProvider, extendTheme, Container } from "native-base";
+
+import { VStack, Box, Divider, Image, AspectRatio, Stack, HStack, Text, Center, Heading } from "native-base";
 // import NavigationTab from "./components/NavigationTab";
 
-export default function NewsItems() {
+export default function NewsItems(props) {
+  const news = props.news;
   return (
-    <Center>
-      <Container>
-        <Heading>
-          <Text color="emerald.500"> NewsAnts</Text>
-        </Heading>
-      </Container>
-    </Center>
+    <Box alignItems="center" mt="2" mx="2">
+      <Box
+        maxW="80"
+        maxHeight={"400"}
+        rounded="lg"
+        overflow="hidden"
+        borderColor="coolGray.200"
+        borderWidth="1"
+        _dark={{
+          borderColor: "coolGray.600",
+          backgroundColor: "gray.700",
+        }}
+        _web={{
+          shadow: 2,
+          borderWidth: 0,
+        }}
+        _light={{
+          backgroundColor: "gray.50",
+        }}
+      >
+        <Box>
+          <AspectRatio w="100%" ratio={16 / 9}>
+            <Image source={news.urlToImage} alt="image" />
+          </AspectRatio>
+          <Center
+            bg="violet.500"
+            _dark={{
+              bg: "violet.400",
+            }}
+            _text={{
+              color: "warmGray.50",
+              fontWeight: "700",
+              fontSize: "xs",
+            }}
+            position="absolute"
+            bottom="0"
+            px="3"
+            py="1.5"
+          >
+            PHOTOS
+          </Center>
+        </Box>
+        <Stack p="4" space={3}>
+          <Stack space={2}>
+            <Heading size="md" ml="-1">
+              {news.title}
+            </Heading>
+            <Text
+              fontSize="xs"
+              _light={{
+                color: "violet.500",
+              }}
+              _dark={{
+                color: "violet.400",
+              }}
+              fontWeight="500"
+              ml="-0.5"
+              mt="-1"
+            ></Text>
+          </Stack>
+          <Text fontWeight="400">{news.description}</Text>
+          <HStack alignItems="center" space={4} justifyContent="space-between">
+            <HStack alignItems="center">
+              <Text
+                color="coolGray.600"
+                _dark={{
+                  color: "warmGray.200",
+                }}
+                fontWeight="400"
+              >
+                6 mins ago
+              </Text>
+            </HStack>
+          </HStack>
+        </Stack>
+      </Box>
+    </Box>
   );
 }
