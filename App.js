2f42e3ad6f1d94ec6cb1daf5d97155a707170a8e
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-07-02 18:59:19 +0530

    buttons added in navbar

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { NativeBaseProvider, Container } from "native-base";
-// import NavigationTab from "./components/NavigationTab";
 import News from "./components/News";
-
+import { NativeBaseProvider, Box } from "native-base";
+import NavigationTab from "./components/NavigationTab";
+import { BrowserRouter, Router, Rotes } from "react-router-dom";
 export default function App() {
   return (
     <NativeBaseProvider>
+      <NavigationTab />
+
       <News />
     </NativeBaseProvider>
   );
