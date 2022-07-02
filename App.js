6e6ab0985611cf142a63da8781bb66f4e9221409
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-07-02 14:11:49 +0530

    container added

--- App.js
+++ App.js
@@ -1,17 +1,6 @@
 import React from "react";
-import {
-  Text,
-  Link,
-  HStack,
-  Center,
-  Heading,
-  Switch,
-  useColorMode,
-  NativeBaseProvider,
-  extendTheme,
-  VStack,
-  Box,
-} from "native-base";
+import { Text, Link, HStack, Center, Heading, Switch, useColorMode, NativeBaseProvider, extendTheme, Container } from "native-base";
+// import NavigationTab from "./components/NavigationTab";
 import NativeBaseIcon from "./components/NativeBaseIcon";
 import { Platform } from "react-native";
 
@@ -27,41 +16,19 @@ export const theme = extendTheme({ config });
 export default function App() {
   return (
     <NativeBaseProvider>
-      <Center
-        _dark={{ bg: "blueGray.900" }}
-        _light={{ bg: "blueGray.50" }}
-        px={4}
-        flex={1}
-      >
-        <VStack space={5} alignItems="center">
-          <NativeBaseIcon />
-          <Heading size="lg">Welcome to NativeBase</Heading>
-          <HStack space={2} alignItems="center">
-            <Text>Edit</Text>
-            <Box
-              _web={{
-                _text: {
-                  fontFamily: "monospace",
-                  fontSize: "sm",
-                },
-              }}
-              px={2}
-              py={1}
-              _dark={{ bg: "blueGray.800" }}
-              _light={{ bg: "blueGray.200" }}
-            >
-              App.js
-            </Box>
-            <Text>and save to reload.</Text>
-          </HStack>
-          <Link href="https://docs.nativebase.io" isExternal>
-            <Text color="primary.500" underline fontSize={"xl"}>
-              Learn NativeBase
-            </Text>
-          </Link>
-          <ToggleDarkMode />
-        </VStack>
+      {/* <NavigationTab /> */}
+      <Center>
+        <Container>
+          <Heading>
+            A component library for the
+            <Text color="emerald.500"> React Ecosystem</Text>
+          </Heading>
+          <Text mt="3" fontWeight="medium">
+            NativeBase is a simple, modular and accessible component library that gives you building blocks to build you React applications.
+          </Text>
+        </Container>
       </Center>
+      ;
     </NativeBaseProvider>
   );
 }
@@ -72,13 +39,7 @@ function ToggleDarkMode() {
   return (
     <HStack space={2} alignItems="center">
       <Text>Dark</Text>
-      <Switch
-        isChecked={colorMode === "light"}
-        onToggle={toggleColorMode}
-        aria-label={
-          colorMode === "light" ? "switch to dark mode" : "switch to light mode"
-        }
-      />
+      <Switch isChecked={colorMode === "light"} onToggle={toggleColorMode} aria-label={colorMode === "light" ? "switch to dark mode" : "switch to light mode"} />
       <Text>Light</Text>
     </HStack>
   );
